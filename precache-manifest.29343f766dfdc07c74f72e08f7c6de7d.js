e0fc0f0bb758885a4795757bc53778556d9c577c
Kintis <theodoros.kintis@accenture.com>
2018-11-02 20:46:20 +0200

    Updates

---
+++ precache-manifest.29343f766dfdc07c74f72e08f7c6de7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/live/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "779ed593e80bdf439902",
+    "url": "/live/static/js/runtime~main.779ed593.js"
+  },
+  {
+    "revision": "0234a45d842133094c0a",
+    "url": "/live/static/js/main.0234a45d.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/live/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "0234a45d842133094c0a",
+    "url": "/live/static/css/main.1fe728f7.chunk.css"
+  },
+  {
+    "revision": "8892716f68a657d2b6857ffa91b42315",
+    "url": "/live/index.html"
+  }
+];
